b4271ebaeb61263c68b3683de5901ebc0feb6de2
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-08-20 22:35:52 -0400

    longwords alternate

--- longwords.py
+++ longwords.py
@@ -1,16 +1,36 @@
+# inp = input('Give me a word: ')
+
+# vowels = 'aeiouyAEIOUY'
+# prev_letter = ''
+
+# new_string = ''
+
+# for letter in inp:
+#   if letter in vowels:
+#     if letter == prev_letter:
+#       new_string += letter * 4
+#     else:
+#       new_string += letter
+#   else:
+#     new_string += letter
+
+#   prev_letter = letter
+
+# print(new_string)
+
 inp = input('Give me a word: ')
 
-vowels = 'aeiouy'
+vowels = 'aeiouyAEIOUY'
 
 new_string = ''
 
-for (first, second) in inp:
-  if first == second:
-    if first in vowels:
-      new_string += first * 4
+for index in range(len(inp)):
+  if inp[index] in vowels and (index + 1) < len(inp):
+    if inp[index] == inp[index + 1]:
+      new_string += inp[index] * 4
     else:
-      new_string += first
-  else: 
-    new_string += first
+      new_string += inp[index]
+  else:
+    new_string += inp[index]
 
 print(new_string)
